2309f66e46fb2cdda45e4bd2d14289b3a2db40f2
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-06-14 20:21:07 +0100

    Create eleven.js

---
+++ webversion/eleven.js
@@ -0,0 +1,11 @@
+/*
+ * eleven.js
+ * Description: Eleven programming language web implementation
+ * Created on: Friday 14th June 2024 @ 20:21
+ * Author(s): Vincent392
+ */
+
+void init11pl() {
+    Console.log("Eleven Programming Language");
+    Console.log("WEB EDITION");
+}
