e93783ed43aeef9841083e0d13ba4db87d6ac840
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-06-14 21:14:08 +0100

    Update eleven.js

--- webversion/eleven.js
+++ webversion/eleven.js
@@ -2,14 +2,14 @@
  * eleven.js
  * Description: Eleven programming language web implementation
  * Created on: Friday 14th June 2024 @ 20:21
- * Author(s): Vincent392
+ * Author(s): Vincent392, datkat21 (Kat21)
  */
 
-void init11pl() {
+function init11pl() {
     console.log("Eleven Programming Language");
     console.log("WEB EDITION");
 }
 
-void print(string logtext) {
+function print(logtext) {
     console.log(logtext);
 }
