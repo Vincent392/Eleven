42f26da34339b2b29979b39cade3fb3103502056
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-06-15 12:21:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,25 @@
 echo please, have a JDK setup ready to go with gcc or something before running.
 echo press any key to continue
 read junk
+echo Compiling Java Version (1/4)...
 cd jvmversion/src
 javac *.java *.class
 # not the cleanest system but whatever
 cd ..
 mkdir jar
-cd src
-mv *.class ../jar
-cd ..
 cd jar
-jar cvfe eleven-lang.jar
+mkdir com/eleven
+cd ..
+cd src
+mv *.class ../jar/com/eleven
+cd ..
+mkdir build
+cd jar
+jar cvfe eleven-lang.jar com.eleven.message ./com/eleven/*.class
+# refrence code from LWJBCC-pkg:
+# jar cvfe %cmd00%.jar %cmd12%.%cmd13%.%cmd14%.%cmd11% ./%cmd12%/%cmd13%/%cmd14%/*.class
+#anyways
+mv eleven-lang.jar ../build
+cd ..
+echo Java version compiled
+cd ..
