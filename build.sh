cebd932c05ccbf25556c9f56f03f0b684eb17421
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-06-15 01:07:13 +0100

    Update

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Eleven Build Script
+# For Linux/Unix systems
+echo please, have a JDK setup ready to go with gcc or something before running.
+echo press any key to continue
+read junk
+cd jvmversion/src
+javac *.java *.class
+# not the cleanest system but whatever
+cd ..
+mkdir jar
+cd src
+mv *.class ../jar
+cd ..
+cd jar
+jar
