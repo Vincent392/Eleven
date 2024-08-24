f7622dec3cd71e0b015dc4150b3ab0d9f20b394d
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-08-24 22:07:23 +0100

    eleven.h

---
+++ lowlevelversion/src/eleven.h
@@ -0,0 +1,8 @@
+/*
+* eleven.h
+* Eleven Programming Language Low-Level Version (EPLLLV) header file
+* Author(s): Vincent392
+* GPL V3 Licensed, if you paid for this, you were scammed.
+*/
+
+#include "main/eleven.c"
